98f2390f683bbddbc4f56b64a441b93725c7fc38
Adam Cooke <adam@atechmedia.com>
2009-08-26 15:46:22 +0100

    big upgrade to markdown processor

--- lib/useful_utils/flavoured_markdown.rb
+++ lib/useful_utils/flavoured_markdown.rb
@@ -4,21 +4,15 @@ module UsefulUtils
     def afm(content, options = {}, &block)
       options[:preserve] = options[:preserve].nil? ? true : options[:preserve]
       options[:sanitize] = options[:sanitize].nil? ? false : options[:sanitize]
-      options[:remove_quote_lines] = options[:remove_quote_lines].nil? ? true : options[:remove_quote_lines]
       options[:format_inline_images] = options[:format_inline_images].nil? ? false : options[:format_inline_images]
       options[:markup] ||= :markdown
-
-      content = sanitize(content) if options[:sanitize]
-
-      if options[:remove_quote_lines]
-        content = content.gsub(/On(.*) wrote:/, '')
-      end
-
+      
+      ## Format images neatly by plopping them into a P tag
       if options[:format_inline_images]
         content.gsub!(/!\[Image(\d+)?\]\((.*)\)/) { content_tag(:p, image_tag($2, :alt => "Image", :width => $1), :class => "image") }
       end
-
-      content = content.strip.chomp
+      
+      content = content.gsub(/[&"<]/) { |special| HTML_ESCAPE[special] }
       
       content = case options[:markup].to_sym
       when :markdown then BlueCloth.new(content)
@@ -27,8 +21,35 @@ module UsefulUtils
       else
         content
       end
-            
+      
       content = (content.respond_to?(:to_html) ? content.to_html : content)
+      
+      ## Extract all the preblocks before we do stuff like this...
+      extractions = {}
+      content.gsub!(%r{<pre>.*?</pre>}m) do |match|
+        md5 = Digest::MD5.hexdigest(match)
+        extractions[md5] = match
+        "{afm-extraction-#{md5}}"
+      end
+
+      ## Sanaitize the content if appropriate
+      if options[:sanitize]
+        content = sanitize(content)
+      end
+      
+      if options[:autolink]
+        content = auto_link(content)
+      end
+
+      ## Prevent italic works from ending up with an italic word in the middle.
+      content.gsub!(/(^(?! {4}|\t)\w+_\w+_\w[\w_]*)/) do |x|
+        x.gsub('_', '\_') if x.split('').sort.to_s[0..1] == '__'
+      end
+      
+      ## Re-insert the pres
+      content.gsub!(/\{afm-extraction-([0-9a-f]{32})\}/) do
+        "\n\n" + extractions[$1].gsub(/(#{ERB::Util::HTML_ESCAPE.values.join('|')})/) { |special| ERB::Util::HTML_ESCAPE.invert[special] }
+      end
 
       if options[:preserve]
         content = preserve(content)
